a525998d78965c27b50aa7d23e391fe2050547d7
N0ViP <01.xx.vip@gmail.com>
2025-02-22 23:18:00 +0100

    start putting pixels

--- main.c
+++ main.c
@@ -5,7 +5,20 @@ typedef struct	s_mlx
 {
 	void	*mlx;
 	void	*win;
+	void	*img;
+	char	*add;
+	int		bpp;
+	int		line_len;
+	int		endian;
 }	t_mlx;
+
+void	put_pixel(t_mlx *mlx, int x, int y, int color)
+{
+	unsigned int	*offset;
+
+	offset = (unsigned int *) (mlx->add + (y * mlx->line_len) + (x * (mlx->bpp / 8)));
+	*offset = color;
+}
 
 int main()
 {
@@ -13,5 +26,16 @@ int main()
 
 	mlx.mlx = mlx_init();
 	mlx.win = mlx_new_window(mlx.mlx, 600, 400, "fract-ol");
+	mlx.img = mlx_new_image(mlx.mlx, 600, 400);
+	mlx.add = mlx_get_data_addr(mlx.img, &mlx.bpp, &mlx.line_len, &mlx.endian);
+	for (int j = 0; j < 200; j++)
+	{
+		for (int i = 0; i < 300; i++)
+		{
+			if (i < j)
+				put_pixel(&mlx, i, j, 0XFFFF0000);
+		}
+	}
+	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
 	mlx_loop(mlx.mlx);
 }
