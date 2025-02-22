3a6041ec50ae2991ece3556ebbc1b2843eeeda19
N0ViP <01.xx.vip@gmail.com>
2025-02-23 00:44:18 +0100

    afs

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <complex.h>
 #include <mlx.h>
 
 typedef struct	s_mlx
@@ -20,6 +21,28 @@ void	put_pixel(t_mlx *mlx, int x, int y, int color)
 	*offset = color;
 }
 
+int	ft_check_if_mandelbort(int x, int y)
+{
+	double complex	z0;
+	double complex	z;
+	double complex	tmp;
+	int				i;
+
+	z0 = 0;
+	z = x + y * I;
+	tmp = z;
+	i = 0;
+	while (i < 100)
+	{
+		tmp = (tmp * tmp) + z;
+		if (tmp >= (double)1000000000)
+			return (1);
+		i++;
+	}
+	return (0);
+
+}
+
 int main()
 {
 	t_mlx  mlx;
@@ -28,12 +51,12 @@ int main()
 	mlx.win = mlx_new_window(mlx.mlx, 600, 400, "fract-ol");
 	mlx.img = mlx_new_image(mlx.mlx, 600, 400);
 	mlx.add = mlx_get_data_addr(mlx.img, &mlx.bpp, &mlx.line_len, &mlx.endian);
-	for (int j = 0; j < 200; j++)
+	for (int j = 0; j < 400; j++)
 	{
-		for (int i = 0; i < 300; i++)
+		for (int i = 0; i < 600; i++)
 		{
-			if (i < j)
-				put_pixel(&mlx, i, j, 0XFFFF0000);
+			if (!ft_check_if_mandelbort(i, j))
+				put_pixel(&mlx, i, j, 0X00FFFFFF);
 		}
 	}
 	mlx_put_image_to_window(mlx.mlx, mlx.win, mlx.img, 0, 0);
